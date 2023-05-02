ce62e1fb59b904b2138d4a249e251701367f16b5
BekxFR <104771705+BekxFR@users.noreply.github.com>
2023-05-02 17:55:49 +0200

    merge main

--- site/cgi-bin/post.php
+++ site/cgi-bin/post.php
@@ -6,30 +6,35 @@
 </head>
 <body>
     <?php
-    // Vérifie si le formulaire a été soumis
-    if (isset($_POST['nom']) && isset($_POST['prenom'])) {
-        // Affiche les valeurs dans les champs du formulaire
-        echo '<form action="post.php" method="post">
-                  <label for="nom">Nom :</label>
-                  <input type="text" name="nom" id="nom" value="' . htmlspecialchars($_POST['nom']) . '">
-                  <br>
-                  <label for="prenom">Prénom :</label>
-                  <input type="text" name="prenom" id="prenom" value="' . htmlspecialchars($_POST['prenom']) . '">
-                  <br>
-                  <input type="submit" value="Envoyer">
-              </form>';
-    } else {
-        // Affiche le formulaire vide
-        echo '<form action="post.php" method="post">
-                  <label for="nom">Nom :</label>
-                  <input type="text" name="nom" id="nom">
-                  <br>
-                  <label for="prenom">Prénom :</label>
-                  <input type="text" name="prenom" id="prenom">
-                  <br>
-                  <input type="submit" value="Envoyer">
-              </form>';
-    }
+        // Vérifie si le formulaire a été soumis
+        if (isset($_POST['nom']) && isset($_POST['prenom'])) {
+            // Affiche les valeurs dans les champs du formulaire
+            echo '<form action="post.php" method="post">
+                      <label for="nom">Nom :</label>
+                      <input type="text" name="nom" id="nom" value="' . htmlspecialchars($_POST['nom']) . '">
+                      <br>
+                      <label for="prenom">Prénom :</label>
+                      <input type="text" name="prenom" id="prenom" value="' . htmlspecialchars($_POST['prenom']) . '">
+                      <br>
+                      <input type="submit" value="Envoyer">
+                  </form>';
+        } else {
+            // Affiche le formulaire vide
+            echo '<form action="post.php" method="post">
+                      <label for="nom">Nom :</label>
+                      <input type="text" name="nom" id="nom">
+                      <br>
+                      <label for="prenom">Prénom :</label>
+                      <input type="text" name="prenom" id="prenom">
+                      <br>
+                      <input type="submit" value="Envoyer">
+                  </form>';
+        }
+        
+        // Affiche les paramètres de la requête
+        echo "<p>Paramètres de la requête : " . $_SERVER['QUERY_STRING'] . "</p>";
+		echo "<p>" . var_dump($_POST['nom']) . "</p>";
+		echo "<p>" . var_dump($_POST['prenom']) . "</p>";
     ?>
 </body>
 </html>
